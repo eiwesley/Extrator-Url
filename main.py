6126b677c7d1a7ac166984a2736f5c15c10932d2
Wesley Medeiros <90868789+eiwesley@users.noreply.github.com>
2023-06-06 20:24:44 -0300

    :sparkles: feat: 06. URL com múltiplos parâmetros

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
-url = "bytebank.com/cambio?moedaDestino=dolar&moedaOrigem=real"
+url = "bytebank.com/cambio?moedaDestino=dolar&quantidade=100&moedaOrigem=real"
 
+# Separa base e parâmetros
 indice_interrogacao = url.find('?')
 url_base = url[:indice_interrogacao]
-
 url_parametros = url[indice_interrogacao + 1:]
 
+# Busca o valor de um parâmetro
 parametro_busca = "moedaOrigem"
 indice_parametro = url_parametros.find(parametro_busca)
 indice_valor = indice_parametro + len(parametro_busca) + 1
-valor = url_parametros[indice_valor:]
+indice_e_comercial = url_parametros.find('&', indice_valor)
+if indice_e_comercial == -1:
+    valor = url_parametros[indice_valor:]
+else:
+    valor = url_parametros[indice_valor:indice_e_comercial]
 print(valor)
